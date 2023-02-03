675ded1e0d9f74bd5bef14b41d3d2e1a28046d04
satishkumarjanga <124369790+satishkumarjanga@users.noreply.github.com>
2023-02-03 19:21:57 +0530

    Add files via upload

---
+++ modules/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Student Attendance Management System</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/validator.min.js"></script>
+  </head>
+  <body>
+
+  	<div id="wrapper">
+    <div class="overlay"></div>
+
+    <h1>ADIKAVI NANNAYA UNIVERSITY</h1>
+    <style>
+      h1{
+        margin-top:0;
+        padding: 20px;
+        background-color:#72FD16;
+        color:#2D1060;
+        text-align: center;
+      }
+      .sidebar-wrapper{
+        color:#601010;
+      }
+    </style>
